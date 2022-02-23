7dd0da969d5d7f680be70813970086dbfe4882fe
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-02-23 04:29:34 +0000

    new program is initiated

---
+++ p2original.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void input_triangle(float *x1,float *y1,float *x2,float *y2,float *x3,float  *x3)
+{
+  
+}
+int is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
+{
+  
+}
+void ouptut(float x1,float y1,float x2,float y2,float x3,float y3)
+{
+  
+}
+int main()
+{
+  float x1,y1,x2,y2,x3,y3;
+  input_triangle(&x1,&y1,x2,)
+}
